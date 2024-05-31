81e3b82246137296672335f9ab7bf42c0481e03d
Nicholas <35278540+Xantiem@users.noreply.github.com>
2024-05-31 09:51:40 +0200

    Added macro

---
+++ main.py
@@ -0,0 +1,11 @@
+with open("econ_concepts.csv") as fp:
+    lines = fp.readlines()[2:]
+
+with open("macro.html", "w") as dat:
+    dat.write("<h1>Micro terminology</h1>")
+
+    for line in lines:
+        data = line.strip("\n").split(";")
+        dat.write("<p><b style=\"color: blue;\">{}</b>: {}</p>".format(data[0], data[1]))
+
+    
